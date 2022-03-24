fec6d65615b8bb9946df057efd06b713167e5542
Gega <gelagzirishvili@gmail.com>
2022-03-24 20:16:28 +0400

    Moved progress bar to its own component

---
+++ src/componenets/progress-bar.component.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import * as config from "SourceRoute/Checkout/Checkout.config";
+
+class ProgressBar extends Component {
+  filterSteps() {
+    return Object.keys(config).filter((key) => key.includes("STEP"));
+  }
+  renderCheckoutSteps = (currentStep = this.props.checkoutStep) => {
+    let rawSteps = this.filterSteps();
+    let stepDisplayText = rawSteps.map(
+      (x) =>
+        x.split("_")[0].charAt(0) +
+        x.split("_")[0].slice(1).toString().toLowerCase()
+    );
+    let steps = stepDisplayText.map((step, index) => (
+      <>
+        {index > 0 && index < stepDisplayText.length ? (
+          <div className="baseLine middleLine">
+            {" "}
+            <div
+              className={`${
+                index <= rawSteps.indexOf(currentStep) ? "fillLine" : ""
+              }`}
+            />
+          </div>
+        ) : null}
+        <div className="step">
+          <div className="stepNumber">
+            <div className="stepContent">
+              {rawSteps.indexOf(this.props.checkoutStep) > index ? (
+                <div>&#x2714;</div>
+              ) : (
+                index + 1
+              )}
+            </div>
+
+            <div
+              className={`${
+                index <= rawSteps.indexOf(currentStep) ? "stepNumberFill" : ""
+              }`}
+            />
+          </div>
+          <p className="stepName">{step}</p>
+        </div>
+      </>
+    ));
+    return (
+      <div className="checkoutProgress">
+        <div className={`baseLine line `}>
+          <div className="fillLine"></div>
+        </div>
+        {steps}
+        <div className="baseLine line">
+          <div
+            className={`${
+              rawSteps.indexOf(currentStep) == rawSteps.length - 1
+                ? "fillLastLine"
+                : ""
+            }`}
+          />
+        </div>
+      </div>
+    );
+  };
+  render() {
+    return this.renderCheckoutSteps()
+  }
+}
+
+export default ProgressBar;
